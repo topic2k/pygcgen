b1d056014fe16bdbbb063cceaaa7afa9ab09478b
topic2k <topic2k+git@gmail.com>
2016-09-11 11:27:36 +0200

    avoid an index error

--- pygcgen/generator.py
+++ pygcgen/generator.py
@@ -352,15 +352,15 @@ class Generator:
         return log
 
     def generate_unreleased_section(self):
-        log = ""
+        if not self.filtered_tags:
+            return ""
         now = datetime.datetime.utcnow()
         now = now.replace(tzinfo=dateutil.tz.tzutc())
         head_tag = {"name": self.options.unreleased_label}
         self.tag_times_dict[head_tag["name"]] = now
         unreleased_log = self.generate_log_between_tags(
             self.filtered_tags[0], head_tag)
-        log += unreleased_log
-        return log
+        return unreleased_log
 
     def get_string_for_issue(self, issue):
         '''
