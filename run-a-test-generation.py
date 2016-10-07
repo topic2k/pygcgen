93d19cd3af5efa44c43cd7020bad5332cd145f8f
topic2k <topic2k+git@gmail.com>
2016-10-07 18:51:42 +0200

    change test-run

--- run-a-test-generation.py
+++ run-a-test-generation.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
 
-
 from pygcgen import ChangelogGenerator
-from pygcgen.__main__ import run_gui
 
-# run_gui()
-# exit()
 
 options = [
-    "-v",
+    # "-h",
+    # "--options-file", ".pygcgen_example",
+    # "-u", "topic2k",
+    # "-p", "pygcgen",
+    # '-s', "**Questions:**", "question", "Question",
+    # '-s', "**Future-Requests:**", "feature-request",
+    # '--section', '**Important changes:**', 'notice',
+    # '-s', "**Implemented enhancements:**", "enhancement", "Enhancement",
+    # '-s', "**Fixed bugs:**", "bug", "Bug",
+    # "-v",
+    # "--quiet",
+    "--no-overwrite",
+    # "--between-tags", "v5.1.1",# "v0.1.1",
     "--with-unreleased",
+    # "--future-release", "v0.2.0",
+    "--exclude-labels", "duplicate", "Duplicate",
+                        "invalid", "Invalid",
+                        "wontfix", "Wontfix",
+                        "question", "Question",
+                        "hide in changelog",
+    # "--tag-separator", " ---\n\n",
 ]
 
 chagen = ChangelogGenerator(options)
